f5b0105ac813f9468b8cbc8ee48648ffacad25ed
LouisYLWang <louis.yl.wang@outlook.com>
2021-02-23 22:42:30 -0800

    updated oag international seat and flight

--- src/transform_load.py
+++ src/transform_load.py
@@ -48,7 +48,8 @@ def transform_airport(country, state):
     return airport
 
 def transform_flight(airport):
-    flight = pd.read_csv(os.path.join(DATASET_DIR,'flightlist_20210101_20210131.csv.gz'), compression='gzip', header=0, sep=',', error_bad_lines=False)
+    # extract data from zenodo, TODO: make extraction automated on lambda function
+    flight = pd.read_csv("https://zenodo.org/record/4485741/files/flightlist_20210101_20210131.csv.gz", compression='gzip', header=0, sep=',', error_bad_lines=False)
     flight['date'] = flight['day'].map(lambda x: x.split()[0])
     flight = flight[["origin", "destination", "date"]]
     flight = flight.dropna(subset=['origin', 'destination'])
@@ -73,6 +74,7 @@ def load_country():
             cur.execute("INSERT INTO country (country_name, country_code) values(%s,%s)", 
             [row.Name, row.Code])
         conn.commit()
+    print("Load country finished!") 
 
 # load states 
 def load_us_state():
@@ -87,6 +89,7 @@ def load_us_state():
             cur.execute("INSERT INTO us_state (us_state_name, us_state_code) values(%s,%s)", 
             [row.State, row.Code])
         conn.commit()
+    print("Load us_state finished!") 
 
 # load airport 
 def load_airport(country, state):
@@ -142,30 +145,80 @@ def load_us_state_vaccination(state):
 def load_world_vaccination(country):
     data = pd.read_csv("https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/vaccinations/vaccinations.csv")
     data = data[data["location"].isin(country["Name"])]
-    state_name = pd.Index(state["Name"])
-    data["state_id"] = data["location"].map(lambda x: state_name.get_loc(x) + 1)
+    country_name = pd.Index(country["Name"])
+    data["country_id"] = data["location"].map(lambda x: country_name.get_loc(x) + 1)
     today_data = data
     #next line is used for daily update
     #today_data = data[data["date"] == datetime.datetime.now().strftime('%Y-%m-%d')]
     today_data = today_data.where(today_data.notnull(), None)
     if len(today_data) > 1:
-        today_data = today_data[["state_id", "people_fully_vaccinated", "people_vaccinated_per_hundred", "date"]]
-        us_vac_list = today_data.values.tolist()
+        today_data = today_data[["country_id", "people_fully_vaccinated", "people_vaccinated_per_hundred", "date"]]
+        world_vac_list = today_data.values.tolist()
         try:
             conn = pymysql.connect(host=RDS_HOST, user=DB_USERNAME, passwd=PASSWORD, database=DB_NAME)
         except:
             print("ERROR: Unexpected error: Could not connect to MySQL instance.")
             
         with conn.cursor() as cur:
-            cur.executemany("INSERT INTO vaccine_us (us_state_id, number_people_vaccinated, percentage_people_vaccinated, date) values (%s, %s, %s, %s)", us_vac_list)
+            cur.executemany("INSERT INTO vaccine_world (country_id, number_people_vaccinated, percentage_people_vaccinated, date) values (%s, %s, %s, %s)", world_vac_list)
             conn.commit()
-    print("Load us state vaccination finished!")
+    print("Load world vaccination finished!")
 
-    # data = pd.read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/%i-%i-2021.csv".format([current_time.month, current_time.day]))
-    # "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/{0}-{1}-2021.csv".format(current_time.month, current_time.day)
+SEAT_MONTH = 'Actual Global Scheduled Seats by Month - Last 12 months'
+FLIGHT_MONTH = 'Actual Global Scheduled Flights by Month - Last 12 months'
+#SEAT_WEEK = 'Actual Global Scheduled Seats by week - Last 8 weeks'
+#FLIGHT_WEEK= 'Actual Global Scheduled Flights by Week - Last 8 weeks'
+
+def transform_flight_and_seat(datasource):
+    if datasource == "seat":
+        title = SEAT_MONTH
+        sheet_name = " Seats"
+    elif datasource == "flight":
+        title = FLIGHT_MONTH
+        sheet_name = "Flights"
+    data = pd.read_excel(
+        "https://f.hubspotusercontent30.net/hubfs/490937/Coronavirus%20Web%20Page%202020/coronavirus-tracking-charts-220221/OAG-WEEKLY-TRACKER-22-February-2021.xlsx",
+        sheet_name=sheet_name
+    )        
+    start = data[data.iloc[:, 0] == title].index[0] + 1
+    data = data.iloc[start: start + 17, :]
+    df = pd.DataFrame(data.iloc[2:, 1:13])
+    df.columns = list(data.iloc[0, :13][1:13])
+    df.index = list(data.iloc[:, 0])[2:]
+    df = df.stack().reset_index()
+    df.columns = ["country", "date", datasource + "_number"]
+    df.replace({"USA": "United States",
+                "South Korea": "Korea, Republic of" ,
+                "UAE": "United Arab Emirates"})  
+    return df
+
+# datasource[string], can be "seat"/"flight"
+def load_internation_flight_and_seat(datasource, country):
+    col_name = datasource + "_number"
+    data = transform_flight_and_seat(datasource)
+    data = data[data["country"].isin(country["Name"])]
+    country_name = pd.Index(country["Name"])
+    data["country_id"] = data["country"].map(lambda x: country_name.get_loc(x) + 1)
+    today_data = data
+    today_data = today_data[["country_id", col_name, "date"]]
+    data_ls = today_data.values.tolist()
+    try:
+        conn = pymysql.connect(host=RDS_HOST, user=DB_USERNAME, passwd=PASSWORD, database=DB_NAME)
+    except:
+        print("ERROR: Unexpected error: Could not connect to MySQL instance.")
+        
+    with conn.cursor() as cur:
+        cur.executemany("INSERT INTO oag_{0}_international (country_id, {1}, date) values (%s, %s, %s)".format(datasource, col_name), data_ls)
+        conn.commit()
+    print("Load internation {0} finished!".format(datasource))
+
 if __name__ == "__main__": 
     country = transform_country()
     state = transform_state()
     airport = transform_airport(country, state)
     load_airport(country, state)
     load_flight(airport)
+    load_us_state_vaccination(state)
+    load_world_vaccination(country)
+    load_internation_flight_and_seat("seat", country)
+    load_internation_flight_and_seat("flight", country)
