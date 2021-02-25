8402af09bb9da805f5ce1cdb2d29f321adce369d
LouisYLWang <louis.yl.wang@outlook.com>
2021-02-25 12:52:45 -0800

    added transform and load for covid comfirmed cases and death

--- src/transform_load.py
+++ src/transform_load.py
@@ -107,7 +107,7 @@ def load_airport(country, state):
         conn.commit()
     print("Load airport finished!") 
 
-def load_flight(airport):
+def load_opensky_flight(airport):
     flight = transform_flight(airport)
     try:
         conn = pymysql.connect(host=RDS_HOST, user=DB_USERNAME, passwd=PASSWORD, database=DB_NAME)
@@ -116,7 +116,7 @@ def load_flight(airport):
 
     with conn.cursor() as cur:
         flight_list = flight[["airport_origin_id", "airport_destin_id", "date"]].values.tolist()
-        cur.executemany("INSERT INTO flight_international (departure_airport_id, destination_airport_id, date) values (%s, %s, %s)", flight_list)
+        cur.executemany("INSERT INTO opensky_flight_international (departure_airport_id, destination_airport_id, date) values (%s, %s, %s)", flight_list)
         conn.commit()
     print("Load interational flight finished!")
 
@@ -164,6 +164,84 @@ def load_world_vaccination(country):
             conn.commit()
     print("Load world vaccination finished!")
 
+# dataset[string]: "confirmed"/"deaths"
+# region[string]: "US"/"global"
+def transform_covid_death_and_comfirm(state, country, dataset, region):
+    us_data_url = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_{0}_US.csv".format(dataset)
+    global_data_url = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_{0}_global.csv".format(dataset)
+    us_data = pd.read_csv(us_data_url)
+    global_data = pd.read_csv(global_data_url)
+    global_drop_columns = ["UID", "code3", "FIPS", "Lat", "Long_"]
+    us_drop_columns = ['UID', 'iso2', 'iso3', 'code3', 'FIPS', 'Admin2', 'Country_Region', 'Lat', 'Long_', 'Combined_Key']
+    if dataset == "deaths":
+        global_drop_columns.append("Population")
+        us_drop_columns.append('Population')
+    if region == "US":
+        us_data = us_data.drop(columns=us_drop_columns).groupby(['Province_State']).sum()
+        return us_data
+    elif region == "global":
+        # add us to global
+        us_data = us_data.drop(columns=global_drop_columns).groupby(['iso2'], as_index=False).sum()    
+        us_data_ls = us_data[us_data["iso2"] == "US"].values.tolist()[0]
+        global_data = global_data.drop(columns= ["Province/State", "Lat", "Long"])
+        global_data.loc[len(global_data)] = us_data_ls
+        global_data = global_data.replace({
+                "US": "United States",
+                "Korea, South": "Korea, Republic of"})  
+        global_data = global_data[global_data['Country/Region'].isin(country["Name"])]
+        global_data = global_data.groupby(['Country/Region']).sum()
+        return global_data
+    # data = data[data['Country/Region'].isin(country["Name"])]
+    # data.groupby(['Country/Region']).sum()
+
+def change_date_format(date_str):
+    dt = datetime.datetime.strptime(date_str, '%m/%d/%y')
+    return datetime.date.strftime(dt, "%Y-%m-%d")
+
+def load_covid_death_and_comfirm(state, country, dataset, region):
+    df = transform_covid_death_and_comfirm(state, country, dataset, region)
+    df = df.stack().reset_index()
+    df.columns = ["country/state", 
+                  "date",
+                  "number"]
+    df["date"] = df["date"].map(lambda x: change_date_format(x))
+
+    if region == "global":
+        index = pd.Index(country["Name"]) 
+        df = df[df["country/state"].isin(country["Name"])]
+    elif region == "US":
+        index = pd.Index(state["State"])
+        df = df[df["country/state"].isin(state["State"])]
+
+    df["country/state"] = df["country/state"].map(lambda x: index.get_loc(x) + 1)
+    value_ls = df.values.tolist()
+    # map datasource keywork to sql table name
+    keywords_map = {
+        "confirmed" : "case",
+        "deaths": "death",
+        "US": "state",
+        "global": "country"
+    }
+
+    table_name = "covid_{0}_{1}".format(keywords_map[dataset], keywords_map[region])
+    region_name = "{0}_id".format(keywords_map[region])
+    value_name = "number_of_{0}s".format(keywords_map[dataset])
+
+    try:
+        conn = pymysql.connect(host=RDS_HOST, user=DB_USERNAME, passwd=PASSWORD, database=DB_NAME)
+    except:
+        print("ERROR: Unexpected error: Could not connect to MySQL instance.")
+        
+    with conn.cursor() as cur:
+        cur.executemany("INSERT INTO {0} ({1}, Date, {2}) values (%s, %s, %s)".format(
+            table_name,
+            region_name,
+            value_name
+            ), value_ls)
+        conn.commit()
+    print("Load {0} covid {1} finished!".format(region, dataset))
+
+
 SEAT_MONTH = 'Actual Global Scheduled Seats by Month - Last 12 months'
 FLIGHT_MONTH = 'Actual Global Scheduled Flights by Month - Last 12 months'
 #SEAT_WEEK = 'Actual Global Scheduled Seats by week - Last 8 weeks'
@@ -187,12 +265,12 @@ def transform_flight_and_seat(datasource):
     df.index = list(data.iloc[:, 0])[2:]
     df = df.stack().reset_index()
     df.columns = ["country", "date", datasource + "_number"]
-    df.replace({"USA": "United States",
+    df = df.replace({"USA": "United States",
                 "South Korea": "Korea, Republic of" ,
                 "UAE": "United Arab Emirates"})  
     return df
 
-# datasource[string], can be "seat"/"flight"
+# datasource[string]: "seat"/"flight"
 def load_internation_flight_and_seat(datasource, country):
     col_name = datasource + "_number"
     data = transform_flight_and_seat(datasource)
@@ -217,8 +295,12 @@ if __name__ == "__main__":
     state = transform_state()
     airport = transform_airport(country, state)
     load_airport(country, state)
-    load_flight(airport)
+    load_opensky_flight(airport)
     load_us_state_vaccination(state)
     load_world_vaccination(country)
     load_internation_flight_and_seat("seat", country)
     load_internation_flight_and_seat("flight", country)
+    load_covid_death_and_comfirm(state, country, "deaths", "US")
+    load_covid_death_and_comfirm(state, country, "deaths", "global")
+    load_covid_death_and_comfirm(state, country, "confirmed", "US")
+    load_covid_death_and_comfirm(state, country, "confirmed", "global")
